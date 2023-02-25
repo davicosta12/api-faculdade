57d2e284d0f9e16a40191d5c2f9d5dbb7b89a15a
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2023-02-25 20:24:01 -0300

    mover o projeto de backend para pasta separada

---
+++ Dev_Backend/Models/Departments/Department.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_faculdade.Models.Departments
+{
+    public class Department
+    {
+        public int Cod_Departamento { get; set; }
+        public string Nome_departamento { get; set; }
+    }
+}
---
+++ Dev_Backend/Models/Departments/DepartmentDisciplines.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_faculdade.Models.Departments
+{
+    public class DepartmentDisciplines
+    {
+        public string Nome_departamento { get; set; }
+        public string Nome_Disciplina { get; set; }
+    }
+}
