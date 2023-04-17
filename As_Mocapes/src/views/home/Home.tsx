b4faa9a552c9dc22d74d7341572d39fb22e4240d
leandroalveswork <leandroprogramadoruxdesign@gmail.com>
2023-04-16 23:41:54 -0300

    revisar o navigation e a home

--- As_Mocapes/src/views/home/Home.tsx
+++ As_Mocapes/src/views/home/Home.tsx
@@ -8,6 +8,7 @@ function Home() {
       <div className="half-padding">
         <div className="half-padding">
           <Typography.Title level={3}>Bem-vindo ao SIGA++</Typography.Title>
+          <Typography.Title level={5}>Utilize o menu para iniciar.</Typography.Title>
         </div>
       </div>
     </NavigationWrapper>
