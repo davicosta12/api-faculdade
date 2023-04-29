20e06b72c87ceba2f0814245ae75daac2ddf3c0e
Davi Costa <davi.costa@ajdesenvolvimento.com.br>
2023-04-29 16:04:58 -0300

    ajustrs

--- Dev_Backend/Data/Models/Users/User.cs
+++ Dev_Backend/Data/Models/Users/User.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Dev_Backend.Data.Models.Authentication;
 
 namespace Dev_Backend.Data.Models.Users
 {
@@ -19,15 +20,29 @@ namespace Dev_Backend.Data.Models.Users
 
     public class PostUser
     {
+       [Required]
         public string C_Perfil { get; set; }
+
         public string S_Nome { get; set; }
-        public string S_CPF { get; set; } 
+
+        [Required]
+        public string S_CPF { get; set; }
+
         public string S_RA { get; set; }
+        
         public string C_Sexo { get; set; }
+
         public string S_Nome_Mae { get; set; }
+
         public bool B_E_Ativo { get; set; }
+
         public string S_Email { get; set; }
-        public string  S_Senha { get; set; }
+
+        [Required]
+        public string S_Senha { get; set; }
+
+        public UserConfirmPassword userConfirmPassword { get; set; } = new UserConfirmPassword();
+        
         public bool B_Tem_Senha_Temporaria { get; set; }
     }
 }
